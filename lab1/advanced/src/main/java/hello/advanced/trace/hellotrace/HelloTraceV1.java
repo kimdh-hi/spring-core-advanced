103299b5ad2436e0feeeabae2bc76f40de45ab9f
kimdh-hi <zbeld123@gmail.com>
2021-12-30 18:59:23 +0900

    섹션1 로그추적기v1 구현

---
+++ lab1/advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV1.java
@@ -0,0 +1,57 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV1 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    // 로깅시작 및 실행시간 측정시작
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    // 정상종료 시 로깅
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    // 예외발생 시 로깅
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs(); // 실행시간 측정
+        TraceId traceId = status.getTraceId();
+        if (e == null) { // 정상종료
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(),
+                    resultTimeMs);
+        } else { // 예외발생
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
+                    e.toString());
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append( (i == level - 1) ? "|" + prefix : "| ");
+        }
+        return sb.toString();
+    }
+
+}

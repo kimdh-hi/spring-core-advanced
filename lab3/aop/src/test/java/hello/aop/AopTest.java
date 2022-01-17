87d456e647b5b34b17ffa70d4a96846de89fc885
kimdh-hi <zbeld123@gmail.com>
2022-01-17 12:12:00 +0900

    AOP v1 테스트

--- lab3/aop/src/test/java/hello/aop/AopTest.java
+++ lab3/aop/src/test/java/hello/aop/AopTest.java
@@ -2,13 +2,16 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import({AspectV1.class})
 @Slf4j
 @SpringBootTest
 public class AopTest {
